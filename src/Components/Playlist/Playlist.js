8c9a259250ca82dc7f54001108ae91f63f791f44
nhanbin03 <nhanbin03@gmail.com>
2023-01-19 21:53:41 +0700

    add header for user playlist section

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import TrackList from '../TrackList/TrackList'
+import PlaylistList from '../PlaylistList/PlaylistList';
 
 import './Playlist.css'
 
@@ -18,6 +19,8 @@ class Playlist extends React.Component {
                 <input defaultValue={"New Playlist"} onChange={this.handleNameChange}/>
                 <TrackList tracks={this.props.playlistTracks} onClick={this.props.onRemove} isRemoval={true}/>
                 <button className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
+                <h2>Your Playlists</h2>
+                <PlaylistList />
             </div>
         );
     }
---
+++ src/Components/PlaylistList/PlaylistList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './PlaylistList.css'
+
+class PlaylistList extends React.Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default PlaylistList;
